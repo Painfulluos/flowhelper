e19c22a40ee30d0126f176896195419d0018db76
Painfulluos <painfulluos@gmail.com>
2023-04-14 07:37:25 +0300

    add new funcs
    Теперь изображение создается не в Label, а в Canvas
    1) Добавлено выделение области;
    2) Добавлено закрашивание выделенной области;
    3) Добавлена проверка несохраненных изменений при закрытии программы

--- flowHelper.py
+++ flowHelper.py
@@ -11,6 +11,15 @@ class PyFlowHelper:
 		self.root = Tk()
 		self.image_tabs = Notebook(self.root)
 		self.opened_images = []
+
+		self.selection_top_x = 0
+		self.selection_top_y = 0
+		self.selection_bottom_x = 0
+		self.selection_bottom_y = 0
+
+		self.canvas_for_selection = None
+		self.selection_rect = None
+
 		self.init()
 
 	def init(self):
@@ -48,9 +57,13 @@ class PyFlowHelper:
 		menu_bar.add_cascade(label="File", menu=file_menu)
 
 		edit_menu = Menu(menu_bar, tearoff=0)
-
 		edit_menu.add_command(label="Painting over top and bottom", command=self.painting_over_top_and_bottom)
+		edit_menu.add_command(label="Painting over selection", command=self.painting_current_selection)
 
+		select_menu = Menu(edit_menu, tearoff=0)
+		select_menu.add_command(label="Start selection", command=self.start_area_selection)
+		select_menu.add_command(label="Stop selection", command=self.stop_area_selection)
+		edit_menu.add_cascade(label="Selection menu", menu=select_menu)
 
 		menu_bar.add_cascade(label="Edit", menu=edit_menu)
 
@@ -58,8 +71,13 @@ class PyFlowHelper:
 
 	def draw_widgets(self):
 
-		self.button_open_images = Button(self.root, text="Open images", command=self.open_new_images).pack(pady=1)
-		self.button_fill_top_bot = Button(self.root, text="Painting over top and bottom", command=self.painting_over_top_and_bottom).pack(pady=1)
+		self.button_open_images = Button(self.root, text="Open images", command=self.open_new_images).pack(pady=1, side=TOP)
+		self.button_fill_top_bot = Button(self.root, text="Painting over top and bottom", command=self.painting_over_top_and_bottom).pack(pady=1, side=TOP)
+		self.button_fill_selection = Button(self.root, text="Fill selection", command=self.painting_current_selection).pack(pady=1, side=TOP)
+		self.button_start_selection = Button(self.root, text="Start selection", command=self.start_area_selection).pack(pady=1, side=LEFT)
+		self.buttin_stop_selection = Button(self.root, text="Stop selection", command=self.stop_area_selection).pack(pady=1, side=RIGHT)
+		
+
 
 		self.image_tabs.pack(fill="both", expand=1)
 
@@ -71,19 +89,19 @@ class PyFlowHelper:
 	def add_new_image(self, image_path):
 		image = Image.open(image_path)
 		image_tk = ImageTk.PhotoImage(image)
-		
 		self.opened_images.append([image_path, image])
 
 		image_tab = Frame(self.image_tabs)
-		
-		image_label = Label(image_tab, image=image_tk, anchor="nw")
-		image_label.image = image_tk
-		image_label.pack(side="bottom", fill="both", expand="yes")
+
+		image_canvas = Canvas(image_tab, width=image_tk.width(), height=image_tk.height(), bd=0, highlightthickness=0)
+		image_canvas.image = image_tk
+		image_canvas.create_image(0,0, image=image_tk, anchor="nw")
+		image_canvas.pack(expand="yes")
 
 		self.image_tabs.add(image_tab, text=image_path.split('/')[-1])
 		self.image_tabs.select(image_tab)
 
-		self.resize_current_image(75)
+		# self.resize_current_image(75)
 
 
 	def get_current_working_data(self):
@@ -151,13 +169,16 @@ class PyFlowHelper:
 	def update_image_inside_app(self, current_tab, image):
 		tab_number = self.image_tabs.index(current_tab)
 		tab_frame = self.image_tabs.children[current_tab[current_tab.rfind('!'):]]
-		label = tab_frame.children["!label"]
+		canvas = tab_frame.children["!canvas"]
 
 		self.opened_images[tab_number][1] = image
 
 		image_tk = ImageTk.PhotoImage(image)
-		label.configure(image=image_tk)
-		label.image = image_tk
+
+		canvas.delete('all')
+		canvas.image = image_tk
+		canvas.configure(width=image_tk.width(), height=image_tk.height())
+		canvas.create_image(0,0, image=image_tk, anchor="nw")
 
 		image_path = self.opened_images[tab_number][0]
 		if image_path[-1] != "*":
@@ -203,6 +224,61 @@ class PyFlowHelper:
 		draw.rectangle( rect3, fill="black", outline=outline )
 
 		self.update_image_inside_app(current_tab, image)
+
+	def start_area_selection(self):
+		current_tab = self.image_tabs.select()
+		if not current_tab:
+			return
+		tab_frame = self.image_tabs.children[current_tab[current_tab.rfind("!"):]]
+		canvas = tab_frame.children["!canvas"]
+		
+		self.canvas_for_selection = canvas
+		self.selection_rect = canvas.create_rectangle(
+			self.selection_top_x, self.selection_top_y, 
+			self.selection_bottom_x, self.selection_bottom_y, 
+			dash=(10, 10), fil="", outline="white", width=2
+		)
+
+		canvas.bind("<Button-1>", self.get_selection_start_pos)
+		canvas.bind("<B1-Motion>", self.update_selection_end_pos)
+
+	def get_selection_start_pos(self, event):
+		self.selection_top_x, self.selection_top_y = event.x, event.y
+
+
+	def update_selection_end_pos(self, event):
+		self.selection_bottom_x, self.selection_bottom_y = event.x, event.y
+		if self.canvas_for_selection is not None and self.selection_rect is not None:
+			self.canvas_for_selection.coords(
+				self.selection_rect,
+				self.selection_top_x, self.selection_top_y,
+				self.selection_bottom_x, self.selection_bottom_y
+			)		
+
+
+	def stop_area_selection(self):
+		self.canvas_for_selection.unbind("<Button-1>")
+		self.canvas_for_selection.unbind("<B1-Motion>")
+
+		self.canvas_for_selection.delete(self.selection_rect)
+
+		self.selection_rect = None
+		self.canvas_for_selection = None
+		self.selection_top_x, self.selection_top_y = 0, 0
+		self.selection_bottom_x, self.selection_bottom_y = 0, 0
+
+
+	def painting_current_selection(self):
+		current_tab, path, image = self.get_current_working_data()
+		if not current_tab:
+			return
+		outline = (0,0,0)
+		draw = ImageDraw.Draw(image)
+		draw.rectangle( (self.selection_top_x, self.selection_top_y, self.selection_bottom_x, self.selection_bottom_y), fill="black", outline=outline )
+
+		self.update_image_inside_app(current_tab, image)
+
+
 
 	def unsaved_images(self):
 		for path, _ in self.opened_images:
